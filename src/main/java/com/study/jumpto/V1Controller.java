e2f549a12df5231fa8fb7f00e01fdcc8f3294c9b
P088454 <P088454@SKTP088454PN001.SKT.AD>
2024-10-14 15:56:21 +0900

    springstudy db생성

---
+++ src/main/java/com/study/jumpto/V1Controller.java
@@ -0,0 +1,24 @@
+package com.study.jumpto;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/v1")
+public class V1Controller {
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "hello jump to spring";
+	}
+	
+	@GetMapping("/test")
+	@ResponseBody
+	public String test() {
+		//System.out.println("index");
+		return "index";
+	}
+
+}
---
+++ src/main/java/com/study/jumpto/V1Dto.java
@@ -0,0 +1,14 @@
+package com.study.jumpto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@RequiredArgsConstructor
+public class V1Dto {
+	private String id;
+	private String name;
+}
