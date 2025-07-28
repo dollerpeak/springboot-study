1df4deb3133dea3124e60023476fc19640c8cbb5
dollerpeak <kobold77@empas.com>
2025-07-28 18:02:12 +0900

    faicon filter 추가했음

---
+++ springboot-security/src/main/java/com/shm/TempleteController.java
@@ -0,0 +1,21 @@
+package com.shm;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("/templete")
+@Slf4j
+public class TempleteController {
+
+	@GetMapping({ "", "/" })
+	public String main() {
+		log.info("===> TempleteController");
+		
+		return "/templete";
+	}
+
+}
---
+++ springboot-security/src/main/java/com/shm/common/filter/FaviconFilter.java
@@ -0,0 +1,36 @@
+package com.shm.common.filter;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class FaviconFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+		
+		// 요청 무시하고 로그도 남기지 않음
+		if(httpServletRequest.getRequestURI().equals("/favicon.ico") == true) {
+			((HttpServletResponse) response).setStatus(HttpServletResponse.SC_NO_CONTENT);
+            return; 
+		}
+		
+		chain.doFilter(request, response);
+	}
+
+	
+
+}
