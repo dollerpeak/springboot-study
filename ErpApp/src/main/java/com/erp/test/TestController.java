1727073645adc524c0eeb7289ac541acf84daf25
dollerpeak <kobold77@empas.com>
2026-03-10 19:41:28 +0900

    init

---
+++ ErpApp/src/main/java/com/erp/test/TestController.java
@@ -0,0 +1,20 @@
+package com.erp.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+	@GetMapping("")
+	public String test() {
+		log.info("test");
+		return "ErpApp test page";
+	}
+
+}
